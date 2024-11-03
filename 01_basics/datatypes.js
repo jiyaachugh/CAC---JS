bf605e984d09dc545e155f4ce8ca9ebbee030217
Jiya Chugh <145968547+jiyaachugh@users.noreply.github.com>
2024-11-03 14:21:17 +0000

    datatypes in JS

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+console.log("Jiyaa");
+
+let name = "Jiya";
+let age = 19;
+let isLoggedIn = true;
+
+//Types of DT
+// number +> 2 to power 53
+// big int
+// String => ""
+// boolean => true/false
+// null => stadalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
